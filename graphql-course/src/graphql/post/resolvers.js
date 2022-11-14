4a2cac66f6e9e4acbc413192808400af59ecde47
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2022-11-14 20:10:02 +0000

    chore: input type

--- graphql-course/src/graphql/post/resolvers.js
+++ graphql-course/src/graphql/post/resolvers.js
@@ -2,8 +2,10 @@ const post = async (_, { id }, { getPosts }) => {
   return await getPosts(`/${id}`).then((post) => post.json());
 };
 
-const posts = async (_, __, { getPosts }) =>
-  await getPosts().then((posts) => posts.json());
+const posts = async (_, { input }, { getPosts }) => {
+  const apiFilterTypeDefs = new URLSearchParams(input);
+  return await getPosts(`/?${apiFilterTypeDefs}`).then((posts) => posts.json());
+};
 
 export const postResolvers = {
   Query: {
--- graphql-course/src/graphql/user/resolvers.js
+++ graphql-course/src/graphql/user/resolvers.js
@@ -1,8 +1,11 @@
-const users = async (_, __, { getUsers }) =>
-  await getUsers().then((users) => users.json());
-
 const user = async (_, { id }, { getUsers }) => {
   return await getUsers(`/${id}`).then((user) => user.json());
+};
+
+const users = async (_, { input }, { getUsers }) => {
+  const apiFilterTypeDefs = new URLSearchParams(input);
+
+  return await getUsers(`/?${apiFilterTypeDefs}`).then((users) => users.json());
 };
 
 export const userResolvers = {
