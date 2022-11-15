817f8bfd8844f655f08175697d137713e5f7e26a
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2022-11-15 20:31:54 +0000

    union types

--- graphql-course/src/graphql/post/resolvers.js
+++ graphql-course/src/graphql/post/resolvers.js
@@ -1,5 +1,15 @@
 const post = async (_, { id }, { getPosts }) => {
-  return await getPosts(`/${id}`).then((post) => post.json());
+  const response = await getPosts(`/${id}`);
+  const post = await response.json();
+
+  if (typeof post.id === 'undefined') {
+    return {
+      statusCode: 404,
+      message: 'Post not found!',
+    };
+  }
+
+  return post;
 };
 
 const posts = async (_, { input }, { getPosts }) => {
@@ -11,5 +21,18 @@ export const postResolvers = {
   Query: {
     post,
     posts,
+  },
+  Post: {
+    unixTimestamp: ({ createdAt }) => {
+      const timestamp = new Date(createdAt).getTime() / 1000;
+      return Math.floor(timestamp);
+    },
+  },
+  PostResult: {
+    __resolveType: (obj) => {
+      if (typeof obj.statusCode !== 'undefined') return 'PostNotFoundError';
+      if (typeof obj.id !== 'undefined') return 'Post';
+      return null;
+    },
   },
 };
