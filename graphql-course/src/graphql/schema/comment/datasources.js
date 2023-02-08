93bcb98eebc1a496f60bcdc331c148b69dc8ee02
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-02-08 21:58:00 +0000

    perf: remove extra renders

--- graphql-course/src/graphql/schema/comment/datasources.js
+++ graphql-course/src/graphql/schema/comment/datasources.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-constructor */
 /* eslint-disable space-before-function-paren */
 import { SQLDataSource } from '../../datasource/sql/sql-datasource';
 
@@ -11,12 +12,17 @@ const commentReducer = (comment) => {
 };
 
 export class CommentSQLDataSource extends SQLDataSource {
+  constructor(dbConnection) {
+    super(dbConnection);
+    this.tableName = 'comments';
+  }
+
   async getById(id) {
-    return this.db('comments').where({ id });
+    return this.db(this.tableName).where({ id });
   }
 
   async getByPostId(post_id) {
-    const query = this.db('comments').where({ post_id });
+    const query = this.db(this.tableName).where({ post_id });
     console.log(query.toString());
     const comments = await query;
     return comments.map((comment) => commentReducer(comment));
@@ -29,18 +35,31 @@ export class CommentSQLDataSource extends SQLDataSource {
       comment,
     };
 
-    const exists = await this.db('comments').where(partialComment).first();
+    const exists = await this.db(this.tableName).where(partialComment).first();
 
     if (exists) {
       throw new Error('Comment already exists');
     }
 
-    const created = await this.db('comments').insert(partialComment);
+    const created = await this.db(this.tableName).insert(partialComment);
 
     return {
       id: created[0],
       createdAt: new Date().toISOString(),
       ...partialComment,
     };
+  }
+
+  async batchLoaderCallback(post_ids) {
+    const query = this.db(this.tableName).whereIn('post_id', post_ids);
+    const comments = await query;
+
+    const filteredComments = post_ids
+      .map((post_id) => {
+        return comments.filter((comment) => comment.post_id === post_id);
+      })
+      .map((comment) => comment.map(commentReducer));
+
+    return filteredComments;
   }
 }
